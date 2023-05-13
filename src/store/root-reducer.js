40314dcc66c873f2a3abf63fa67ab2b58e67cdf3
Ersultan Sansyzbay <ersultansansyzbay@MacBook-Air-Ersultan.local>
2023-05-13 13:32:16 +0600

    Редьюсер филтрации

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 
 import { positionsReducer } from "./positions/positions-reducer";
+import { filterReducer } from "./filters/filter-reducer";
 
 export const rootReducer = combineReducers({
     positions: positionsReducer,
+    filters: filterReducer
 });
